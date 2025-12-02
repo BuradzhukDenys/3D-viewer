5c8230a5dff7582fec6c98d912f2c6ce09f15c42
BuradzhukDenys <buradzhuk.denys.clg@chnu.edu.ua>
2025-12-02 19:55:23 +0100

    First test

--- main.cpp
+++ main.cpp
@@ -1,19 +1,66 @@
-#include <glm/glm.hpp>
-#include <GLFW/glfw3.h>
-#include <iostream>
+#include "ViewerWindow.h"
+#include "OpenglRenderSystem.h"
+#include "GeometryPrimitives.h"
+
+#include <glm/gtc/matrix_transform.hpp>
+
+void renderScene(OpenglRenderSystem& rs)
+{
+	static const auto& data = GeometryPrimitives::createCubeVertices();
+	rs.renderTriangleSoup(data);
+}
+
+void moveCube(OpenglRenderSystem& rs, glm::vec3 offset)
+{
+	rs.setViewMatrix(glm::translate(rs.getViewMatrix(), offset));
+}
 
 int main()
 {
-	//Приклад використання GLM
-	glm::vec3 vector(1.0f, 0.0f, 0.0f);
-	std::cout << "Vector: (" << vector.x << ", " << vector.y << ", " << vector.z << ")\n";
+	constexpr int width = 640;
+	constexpr int height = 480;
 
-	//Ініціалізація GLFW
-	if (!glfwInit())
+	OpenglRenderSystem rs;
+	ViewerWindow window("myWindow", width, height);
+
+	window.setKeyCallback([&rs](KeyCode key, Action action, Modifier mods)
+		{
+			if (key == KeyCode::UP)
+				moveCube(rs, { 0, 0.1, 0 });
+
+			if (key == KeyCode::DOWN)
+				moveCube(rs, { 0, -0.1, 0 });
+
+			if (key == KeyCode::LEFT)
+				moveCube(rs, { 0.1, 0, 0 });
+
+			if (key == KeyCode::RIGHT)
+				moveCube(rs, { -0.1, 0, 0 });
+		});
+
+	rs.init();
+	rs.setupLight(0, glm::vec3(0, 5, 0), glm::vec3(1, 0, 0), glm::vec3(0, 1, 0), glm::vec3(0, 0, 1));
+	rs.turnLight(0, true);
+
+	//glm::vec3 cameraPosition(2.0f, 2.0f, -3.0f);
+	glm::vec3 cameraPosition(0.0f, 0.0f, -3.0f);
+	glm::vec3 lookAtCenter(0.0f, 0.0f, 0.0f);
+	glm::vec3 cameraUpVector(0.0f, 1.0f, 0.0f);
+
+	glm::mat4 viewMatrix = glm::lookAt(cameraPosition, lookAtCenter, cameraUpVector);
+	rs.setViewMatrix(viewMatrix);
+
+	glm::mat4 projMatrix = glm::perspective(glm::radians(60.0f), float(width) / float(height), 0.1f, 500.f);
+	rs.setProjectionMatrix(projMatrix);
+
+	while (!glfwWindowShouldClose(window.getGLFWHandle()))
 	{
-		std::cerr << "Failed to initialize GLFW\n";
-		return -1;
+		rs.setViewport(0, 0, window.getWidth(), window.getHeight());
+		rs.clearDisplay(0.5f, 0.5f, 0.5f);
+		renderScene(rs);
+		glfwSwapBuffers(window.getGLFWHandle());
+		glfwWaitEvents();
 	}
-	glfwTerminate();
+
 	return 0;
 }
